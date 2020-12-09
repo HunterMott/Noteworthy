d94a41cc3fc0e48e1ad6fd6bde1a601c47b21bb6
Hunter <huntermott42@gmail.com>
2020-12-09 12:30:32 -0700

    fixed nav buttons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import '../styles/header.css'
+import "../styles/header.css";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav className='navbuttons'>
-      <Button variant="contained" href="/" color="primary">
-        Home
-      </Button>
-      <Button variant="contained" href="/todo" color="primary">
-        To-Do
-      </Button>
-      <Button variant="contained" href="/notes" color="primary">
-        Notes
-      </Button>
+    <nav className="navbuttons">
+      <Link to="/">
+        <Button variant="contained" color="primary">
+          Home
+        </Button>
+      </Link>
+      <Link to="/todo">
+        <Button variant="contained" color="primary">
+          To-Do
+        </Button>
+      </Link>
+      <Link to="/notes">
+        <Button variant="contained" color="primary">
+          Notes
+        </Button>
+      </Link>
     </nav>
   );
 };
